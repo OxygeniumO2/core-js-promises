c3a8c13c40ec0cf69d7afc0fdc3702aaccde7584
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-04-07 16:04:33 +0300

    feat: getAllResult, getAllOrNothing done

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -106,8 +106,12 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises)
+    .then((value) => {
+      return value;
+    })
+    .catch((value) => value);
 }
 
 /**
@@ -122,8 +126,12 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((res) => {
+    return res.map((result) =>
+      result.status === 'fulfilled' ? result.value : null
+    );
+  });
 }
 
 /**
